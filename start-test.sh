7d774ac2f5863eba3a591fe59468e728cc87d8a4
danielmenezesbr <danielmenezes@gmail.com>
2023-11-19 15:43:43 +0000

    add graph result

--- start-test.sh
+++ start-test.sh
@@ -5,4 +5,9 @@ hyperfine --runs 30 --warmup 5 \
  --prepare "$PWD/start-app.sh && sleep 5" \
  'curl localhost:8000/pdf-uno/' \
  --prepare "pkill soffice || true" \
- 'curl localhost:8000/pdf-subprocess/'
+ 'curl localhost:8000/pdf-subprocess/' \
+ --export-json result.json
+
+ python plot_whisker.py result.json -o result.png
+
+ npx http-server -y
